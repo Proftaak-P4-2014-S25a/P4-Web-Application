c53456699d12d4019cde7b65d123d936c71e4e9b
niekvdb <niek_1993@hotmail.com>
2014-05-22 13:44:25 +0200

    LOLOLO

---
+++ Social Media Events/WebApplication SME/Registration.aspx.cs
@@ -0,0 +1,64 @@
+﻿//Reservation code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Net.Mail;
+using System.Net;
+
+namespace WebApplication_SME
+{
+    public partial class Reservation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+            {
+                Login.Text = "Logout";
+            }
+            Submit.Enabled = false;
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            //**** Send data to database ****//
+            DatabaseMngr dbmngr = new DatabaseMngr();
+
+            //*************** TODO ************************
+
+            //Send email to new user
+
+            //*************** TODO ************************
+
+            SmtpClient smtpClient = new SmtpClient("serveradress", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential("email@test.com", "password");
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+
+            //Creating the mail message
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("email@test.com");
+            mail.To.Add(new MailAddress(Email.Text));
+            mail.Subject = "Validate your email address";
+            mail.Body = "Thank you for registering with Social Media Events!" + System.Environment.NewLine +
+                "Please check the following data you entered, if anything is incorrect please contact: customers@socialmediaevents.com" + System.Environment.NewLine +
+                "Name: " + Name.Text + System.Environment.NewLine +
+                "Street: " + Street.Text + System.Environment.NewLine +
+                "Postal code: " + PostalCode.Text + System.Environment.NewLine +
+                "City: " + City.Text + System.Environment.NewLine +
+                "Telephone: " + Phone.Text + System.Environment.NewLine +
+                "Bank account number: " + AccountNumber.Text + System.Environment.NewLine +
+                "Social Security Number: " + SSN.Text + System.Environment.NewLine + System.Environment.NewLine +
+                "If this data is correct, please validate your account by visiting this page: " + System.Environment.NewLine +
+                "";
+
+            //Won't work yet
+            //smtpClient.Send(mail);
+            Response.Redirect("index.aspx");
+        }
+    }
+}
