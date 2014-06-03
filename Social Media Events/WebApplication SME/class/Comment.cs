10842f8234fc507958db57538bd4527fdd3ce60d
niekvdb <niek_1993@hotmail.com>
2014-06-03 17:31:45 +0200

    NOG MEER CLASSES

---
+++ Social Media Events/WebApplication SME/class/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_SME
+{
+	public class Comment
+	{
+        public int CommentID { get; set; }
+        public int RFID { get; set; }
+        public int BestandID { get; set; }
+        public DateTime Datum { get; set; }
+        public string Opmerking { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+	}
+}
