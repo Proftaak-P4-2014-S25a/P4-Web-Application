8aa29cb01447c7dd23ef5fc6b885ccc27329a6e0
SanderBoelaars <sanderboelaars@hotmail.com>
2014-06-03 21:28:08 +0200

    +Reservation/LoanItems etc.

---
+++ Social Media Events/WebApplication SME/class/LoanItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_SME
+{
+	public class LoanItem
+	{
+        private int reservationnumber;
+        private List<Material> materials;
+        private DateTime loanDate;
+        private DateTime returnDate;
+        private int amount;
+
+        public int Reservationnumber
+        {
+            get;
+            set;
+        }
+
+        public List<Material> Materials
+        {
+            get;
+            set;
+        }
+
+        public DateTime LoanDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime ReturnDate
+        {
+            get;
+            set;
+        }
+
+        public int Amount
+        {
+            get;
+            set;
+        }
+
+        public LoanItem(int reservationnumber, DateTime loandate, DateTime returndate)
+        {
+            this.Reservationnumber = reservationnumber;
+            this.LoanDate = loandate;
+            this.ReturnDate = returndate;
+        }
+
+        public void AddMaterial(Material material)
+        {
+            Materials.Add(material);
+        }
+
+        public override string ToString()
+        {
+            return "Reservationnumber: "+Reservationnumber+" Loan Date: "+LoanDate+" Return Date: "+ReturnDate+" Amount: "+Amount;
+        }
+	}
+}
---
+++ Social Media Events/WebApplication SME/class/Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_SME
+{
+	public class Reservation
+	{
+        private int reservationnumber;
+        private Campingspot campingspot;
+        private bool hasBeenPaid;
+
+        public int Reservationnumber
+        {
+            get;
+            set;
+        }
+
+        public Campingspot Campingspot
+        {
+            get;
+            set;
+        }
+
+        public bool HasBeenPaid
+        {
+            get;
+            set;
+        }
+
+        public Reservation(int reservation, Campingspot campingspot, bool hasBeenPaid)
+        {
+            this.Reservationnumber = reservation;
+            this.Campingspot = campingspot;
+            this.HasBeenPaid = hasBeenPaid;
+        }
+
+        public override string ToString()
+        {
+            return "Reservationnumber: " + Reservationnumber + " Campingplace: " + Campingspot;
+        }
+	}
+}
