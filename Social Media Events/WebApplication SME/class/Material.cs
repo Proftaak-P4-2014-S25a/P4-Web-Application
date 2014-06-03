ec7ee50da94483174f05b6f49b8d1927c210e0f9
SanderBoelaars <sanderboelaars@hotmail.com>
2014-06-03 17:34:53 +0200

    +Material class

---
+++ Social Media Events/WebApplication SME/class/Material.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_SME
+{
+	public class Material
+	{
+        private string type;
+        private double price;
+        private int amount;
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public double Price
+        {
+            get;
+            set;
+        }
+
+        public int Amount
+        {
+            get;
+            set;
+        }
+
+        public Material(string type, double price, int amount)
+        {
+            this.Type = type;
+            this.Price = price;
+            this.Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return "Type: " + Type + " Price: " + Price + " Amount: " + Amount + "";
+        }
+	}
+}
